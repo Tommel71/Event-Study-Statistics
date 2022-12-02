9b3324151051393bdeffea112d60205e4969d2de
unknown <thomas.n71@gmail.com>
2022-12-02 15:56:57 +0100

    auto

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,11 @@ author = 'Thomas Niedermayer'
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"
 ]
 
+extensions.append('autoapi.extension')
+
+autoapi_type = 'python'
+autoapi_dirs = ["../eventstudystatistics"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
